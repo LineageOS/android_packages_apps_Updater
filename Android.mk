3fc17494eb5621da7bbd652dd8622e686da0c3cf
Gabriele M <moto.falcon.git@gmail.com>
2017-11-30 23:41:00 +0100

    Updater: Android Studio support
    See README-STUDIO.txt for more info.
    
    Change-Id: Ie673bba5618b0f00a585049cd85687a64db2b420

--- Android.mk
+++ Android.mk
@@ -35,3 +35,24 @@ LOCAL_PRIVILEGED_MODULE := true
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 include $(BUILD_PACKAGE)
+
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := UpdaterStudio
+LOCAL_MODULE_CLASS := FAKE
+LOCAL_MODULE_SUFFIX := -timestamp
+updater_system_deps := $(call java-lib-deps,framework)
+updater_system_libs_path := $(abspath $(LOCAL_PATH))/system_libs
+
+include $(BUILD_SYSTEM)/base_rules.mk
+
+.PHONY: copy_updater_system_deps
+copy_updater_system_deps: $(updater_system_deps)
+	$(hide) mkdir -p $(updater_system_libs_path)
+	$(hide) rm -rf $(updater_system_libs_path)/*.jar
+	$(hide) cp $(updater_system_deps) $(updater_system_libs_path)/framework.jar
+
+$(LOCAL_BUILT_MODULE): copy_updater_system_deps
+	$(hide) echo "Fake: $@"
+	$(hide) mkdir -p $(dir $@)
+	$(hide) touch $@

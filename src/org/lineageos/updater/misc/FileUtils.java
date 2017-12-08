9635d99e055bc803471a08b4b50c6ae74b036126
LuK1337 <priv.luk@gmail.com>
2017-12-08 22:05:56 +0100

    Updater: Switch to v4 NotificationCompat
    * v7 is apparently deprecated and Google
      removed it in 8.1.
    
    Change-Id: I502ed127d2094cfb2b2c5b4849d0551ebbe42746

--- src/org/lineageos/updater/misc/FileUtils.java
+++ src/org/lineageos/updater/misc/FileUtils.java
@@ -20,7 +20,7 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.AsyncTask;
-import android.support.v7.app.NotificationCompat;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.view.WindowManager;
 

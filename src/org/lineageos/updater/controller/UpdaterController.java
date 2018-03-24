a72fe1370b772a5ab120324459ef04bedcf9e25d
Gabriele M <moto.falcon.git@gmail.com>
2018-03-24 16:17:20 +0100

    [DNM] Updater: Debug
    Change-Id: I0f9c8db7edf88aa6b8e062cae72856c4828658ab

--- src/org/lineageos/updater/controller/UpdaterController.java
+++ src/org/lineageos/updater/controller/UpdaterController.java
@@ -104,6 +104,8 @@ public class UpdaterController implements Controller {
     private Map<String, DownloadEntry> mDownloads = new HashMap<>();
 
     void notifyUpdateChange(String downloadId) {
+        Log.d(TAG, "Notification: setting status of " + downloadId + " to " +
+                getUpdate(downloadId).getStatus());
         Intent intent = new Intent();
         intent.setAction(ACTION_UPDATE_STATUS);
         intent.putExtra(EXTRA_DOWNLOAD_ID, downloadId);
@@ -111,6 +113,7 @@ public class UpdaterController implements Controller {
     }
 
     void notifyUpdateDelete(String downloadId) {
+        Log.d(TAG, "Notification: deleting " + downloadId);
         Intent intent = new Intent();
         intent.setAction(ACTION_UPDATE_REMOVED);
         intent.putExtra(EXTRA_DOWNLOAD_ID, downloadId);
@@ -118,6 +121,7 @@ public class UpdaterController implements Controller {
     }
 
     void notifyDownloadProgress(String downloadId) {
+        Log.d(TAG, "Notification: download progress of " + downloadId + " has changed");
         Intent intent = new Intent();
         intent.setAction(ACTION_DOWNLOAD_PROGRESS);
         intent.putExtra(EXTRA_DOWNLOAD_ID, downloadId);
@@ -125,6 +129,7 @@ public class UpdaterController implements Controller {
     }
 
     void notifyInstallProgress(String downloadId) {
+        Log.d(TAG, "Notification: install progress of " + downloadId + " has changed");
         Intent intent = new Intent();
         intent.setAction(ACTION_INSTALL_PROGRESS);
         intent.putExtra(EXTRA_DOWNLOAD_ID, downloadId);

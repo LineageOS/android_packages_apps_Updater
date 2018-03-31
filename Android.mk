bb92b1a25b59a13fcc05ac9a300c9ad2b99a9ff8
Bruno Martins <bgcngm@gmail.com>
2018-03-31 11:39:09 +0100

    Updater: Build with AAPT2
    Change-Id: I45553984269bd2375311047b053e01f67349c695

--- Android.mk
+++ Android.mk
@@ -6,7 +6,9 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-LOCAL_STATIC_JAVA_LIBRARIES := \
+LOCAL_USE_AAPT2 := true
+
+LOCAL_STATIC_ANDROID_LIBRARIES := \
     android-support-design \
     android-support-transition \
     android-support-v4 \
@@ -16,20 +18,7 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
     android-support-v7-recyclerview
 
 LOCAL_RESOURCE_DIR := \
-    $(TOP)/frameworks/support/design/res \
-    $(TOP)/frameworks/support/v7/appcompat/res \
-    $(TOP)/frameworks/support/v7/cardview/res \
-    $(TOP)/frameworks/support/v7/preference/res \
-    $(TOP)/frameworks/support/v7/recyclerview/res \
     $(LOCAL_PATH)/res
-
-LOCAL_AAPT_FLAGS := \
-    --auto-add-overlay \
-    --extra-packages android.support.design \
-    --extra-packages android.support.v7.appcompat \
-    --extra-packages android.support.v7.cardview \
-    --extra-packages android.support.v7.preference \
-    --extra-packages android.support.v7.recyclerview
 
 LOCAL_PACKAGE_NAME := Updater
 LOCAL_PRIVILEGED_MODULE := true

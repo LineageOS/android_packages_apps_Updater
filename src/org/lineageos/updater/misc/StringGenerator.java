f30ab67c85a9bb3bc7a4d86e6e12392ed1b2a509
Gabriele M <moto.falcon.git@gmail.com>
2018-04-21 16:31:17 +0200

    Allow to properly translate the download ETA string
    Change-Id: Ie6bbbddbbd9fd7adbc16472ad9c42c1d257413a1

--- src/org/lineageos/updater/misc/StringGenerator.java
+++ src/org/lineageos/updater/misc/StringGenerator.java
@@ -67,20 +67,20 @@ public final class StringGenerator {
         return String.format(getCurrentLocale(context), "%.0f", bytes / 1024.f / 1024.f);
     }
 
-    public static String formatDuration(Context context, long millis) {
+    public static String formatETA(Context context, long millis) {
         final long SECOND_IN_MILLIS = 1000;
         final long MINUTE_IN_MILLIS = SECOND_IN_MILLIS * 60;
         final long HOUR_IN_MILLIS = MINUTE_IN_MILLIS * 60;
         Resources res = context.getResources();
         if (millis >= HOUR_IN_MILLIS) {
             final int hours = (int) ((millis + 1800000) / HOUR_IN_MILLIS);
-            return res.getQuantityString(R.plurals.duration_hours, hours, hours);
+            return res.getQuantityString(R.plurals.eta_hours, hours, hours);
         } else if (millis >= MINUTE_IN_MILLIS) {
             final int minutes = (int) ((millis + 30000) / MINUTE_IN_MILLIS);
-            return res.getQuantityString(R.plurals.duration_minutes, minutes, minutes);
+            return res.getQuantityString(R.plurals.eta_minutes, minutes, minutes);
         } else {
             final int seconds = (int) ((millis + 500) / SECOND_IN_MILLIS);
-            return res.getQuantityString(R.plurals.duration_seconds, seconds, seconds);
+            return res.getQuantityString(R.plurals.eta_seconds, seconds, seconds);
         }
     }
 

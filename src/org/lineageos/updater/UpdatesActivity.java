78e69c05d8bad0d8d69b5e48850e1e8452bfd669
Gabriele M <moto.falcon.git@gmail.com>
2018-05-28 16:40:07 +0200

    Remove LegacySupport class
    This was meant to provide an upgrade path from the old app. It's
    been almost a year now, this class is no longer needed.
    
    Change-Id: I17c82e5877b275c5ceb3caefa28066abc170f095

--- src/org/lineageos/updater/UpdatesActivity.java
+++ src/org/lineageos/updater/UpdatesActivity.java
@@ -55,7 +55,6 @@ import org.lineageos.updater.controller.UpdaterService;
 import org.lineageos.updater.download.DownloadClient;
 import org.lineageos.updater.misc.BuildInfoUtils;
 import org.lineageos.updater.misc.Constants;
-import org.lineageos.updater.misc.LegacySupport;
 import org.lineageos.updater.misc.StringGenerator;
 import org.lineageos.updater.misc.Utils;
 import org.lineageos.updater.model.UpdateInfo;
@@ -243,20 +242,11 @@ public class UpdatesActivity extends UpdatesListActivity {
         boolean newUpdates = false;
 
         List<UpdateInfo> updates = Utils.parseJson(jsonFile, true);
-
-        List<String> importedNotAvailableOnline = LegacySupport.importDownloads(this, updates);
-
         List<String> updatesOnline = new ArrayList<>();
         for (UpdateInfo update : updates) {
             newUpdates |= controller.addUpdate(update);
             updatesOnline.add(update.getDownloadId());
         }
-
-        if (importedNotAvailableOnline != null) {
-            updatesOnline.removeAll(importedNotAvailableOnline);
-            controller.setUpdatesNotAvailableOnline(importedNotAvailableOnline);
-        }
-
         controller.setUpdatesAvailableOnline(updatesOnline, true);
 
         if (manualRefresh) {

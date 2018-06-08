40cae57d9fed34953a58b721c166bbf2715cfa13
LuK1337 <priv.luk@gmail.com>
2018-06-09 00:10:59 +0200

    Updater: Add property that allows us to downgrade
    Change-Id: I6916639f05c52ae198f0d1656deb23ee5186907f

--- src/org/lineageos/updater/misc/Utils.java
+++ src/org/lineageos/updater/misc/Utils.java
@@ -106,7 +106,8 @@ public class Utils {
     }
 
     public static boolean canInstall(UpdateBaseInfo update) {
-        return update.getTimestamp() >= SystemProperties.getLong(Constants.PROP_BUILD_DATE, 0) &&
+        return (SystemProperties.getBoolean(Constants.PROP_UPDATER_ALLOW_DOWNGRADING, false) ||
+                update.getTimestamp() >= SystemProperties.getLong(Constants.PROP_BUILD_DATE, 0)) &&
                 update.getVersion().equalsIgnoreCase(
                         SystemProperties.get(Constants.PROP_BUILD_VERSION));
     }

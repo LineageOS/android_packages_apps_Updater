eca4ab04974a7721c2ed7a937517b7c8c1fd3946
Chau Truong Thinh <chautruongthinh@gmail.com>
2018-11-25 23:12:31 +0700

    updater: Refracter for SuperiorOS
    Change-Id: I0697c3eb6338a641d610c48225152f62865f7109
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- src/org/lineageos/updater/misc/Constants.java
+++ src/org/lineageos/updater/misc/Constants.java
@@ -34,13 +34,12 @@ public final class Constants {
 
     public static final String PROP_AB_DEVICE = "ro.build.ab_update";
     public static final String PROP_BUILD_DATE = "ro.build.date.utc";
-    public static final String PROP_BUILD_VERSION = "ro.lineage.build.version";
-    public static final String PROP_BUILD_VERSION_INCREMENTAL = "ro.build.version.incremental";
-    public static final String PROP_DEVICE = "ro.lineage.device";
+    public static final String PROP_BUILD_VERSION = "ro.modversion";
+    public static final String PROP_DEVICE = "ro.superior.device";
     public static final String PROP_NEXT_DEVICE = "ro.updater.next_device";
-    public static final String PROP_RELEASE_TYPE = "ro.lineage.releasetype";
-    public static final String PROP_UPDATER_ALLOW_DOWNGRADING = "lineage.updater.allow_downgrading";
-    public static final String PROP_UPDATER_URI = "lineage.updater.uri";
+    public static final String PROP_RELEASE_TYPE = "ro.superior.releasetype";
+    public static final String PROP_UPDATER_ALLOW_DOWNGRADING = "superior.updater.allow_downgrading";
+    public static final String PROP_UPDATER_URI = "superior.updater.uri";
 
     public static final String PREF_INSTALL_OLD_TIMESTAMP = "install_old_timestamp";
     public static final String PREF_INSTALL_NEW_TIMESTAMP = "install_new_timestamp";
--- src/org/lineageos/updater/misc/Utils.java
+++ src/org/lineageos/updater/misc/Utils.java
@@ -146,7 +146,6 @@ public class Utils {
     }
 
     public static String getServerURL(Context context) {
-        String incrementalVersion = SystemProperties.get(Constants.PROP_BUILD_VERSION_INCREMENTAL);
         String device = SystemProperties.get(Constants.PROP_NEXT_DEVICE,
                 SystemProperties.get(Constants.PROP_DEVICE));
         String type = SystemProperties.get(Constants.PROP_RELEASE_TYPE).toLowerCase(Locale.ROOT);
@@ -156,9 +155,7 @@ public class Utils {
             serverUrl = context.getString(R.string.updater_server_url);
         }
 
-        return serverUrl.replace("{device}", device)
-                .replace("{type}", type)
-                .replace("{incr}", incrementalVersion);
+        return serverUrl.replace("{device}", device);
     }
 
     public static String getChangelogURL(Context context) {

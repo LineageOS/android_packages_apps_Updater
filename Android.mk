618ecad81ac01c1b75fe0aca584b5b62436622cd
Michael Bestas <mkbestas@lineageos.org>
2020-05-06 20:48:18 +0300

    Updater: Add privapp permissions xml
    Change-Id: If1953e76859850359a63dc4a4ab50940d76fd59f

--- Android.mk
+++ Android.mk
@@ -24,7 +24,17 @@ LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
+LOCAL_REQUIRED_MODULES := privapp_whitelist_org.lineageos.updater.xml
+
 include $(BUILD_PACKAGE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := privapp_whitelist_org.lineageos.updater.xml
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
 
 
 include $(CLEAR_VARS)

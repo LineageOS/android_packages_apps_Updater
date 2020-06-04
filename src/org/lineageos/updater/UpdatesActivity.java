e93a7989b8073e0a1d84cac17b7e18022b8e5bbc
Alexander Martinz <amartinz@shiftphones.com>
2020-06-04 15:07:45 +0200

    UpdatesActivity: allow sorting from oldest to newest
    We require the user to install builds in order.
    Assume we have 3 updates: A, B and C.
    C depends on B, B depends on A.
    
    Old behavior:
     - sort from newest to oldest
     - show single update
     - result: show update C, skip A and B
    
    New behavior:
     - sort from oldest to newest
     - show single update
     - result: show update A, skip B and C
    
    The old behavior always offered the latest build. If these are incremental
    OTAs, it would not work as the system expects a specific state.
    
    With the new behavior, we do not skip to the latest build, but go step by step
    from oldest to newest.
    
    This will ensure that builds are installed in order and satisfy dependencies.
    
    Change-Id: Ic3f446280f98a2dd15182e713cced9781aa4371f
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- src/org/lineageos/updater/UpdatesActivity.java
+++ src/org/lineageos/updater/UpdatesActivity.java
@@ -282,16 +282,24 @@ public class UpdatesActivity extends UpdatesListActivity {
         List<String> updatesOnline = new ArrayList<>();
 
         if (!updates.isEmpty()) {
-            // sort updates from newest to oldest
-            updates.sort(Comparator.comparingLong(UpdateInfo::getTimestamp).reversed());
+            final boolean sortFromOldestToNewest = getResources().getBoolean(
+                    R.bool.config_sortUpdatesFromOldestToNewest);
+            if (sortFromOldestToNewest) {
+                // sort updates from oldest to newest in order to ensure that
+                // builds are installed in order (important for incremental OTAs)
+                updates.sort(Comparator.comparingLong(UpdateInfo::getTimestamp));
+            } else {
+                // sort updates from newest to oldest
+                updates.sort(Comparator.comparingLong(UpdateInfo::getTimestamp).reversed());
+            }
 
-            final boolean onlyShowLatestUpdate = getResources().getBoolean(
-                    R.bool.config_onlyShowLatestUpdate);
+            final boolean onlyShowSingleUpdate = getResources().getBoolean(
+                    R.bool.config_onlyShowSingleUpdate);
             for (final UpdateInfo update : updates) {
                 newUpdates |= controller.addUpdate(update);
                 updatesOnline.add(update.getDownloadId());
 
-                if (onlyShowLatestUpdate) {
+                if (onlyShowSingleUpdate) {
                     break;
                 }
             }

a844ec40c51401aa9da4f98ac801c2e4a0e98d9a
Michael W <baddaemon87@gmail.com>
2022-02-06 16:40:03 +0100

    Updater: Switch to destination selector
    * Let the user decide where to store the file
    * That way it's not located in /storage/emulated/0/Android/data/...
      ...org.lineageos.updater/files/LineageOS updates/*.zip
      -> The user knows where the file is stored
      -> We don't have to care about WRITE_EXTERNAL_STORAGE etc
    * Remove the cancel button - after closing the file stream we loose
      permission to access it, therefore can't delete it anymore
      -> Let the user handle deletion manually
    
    Change-Id: I50afa403f2803569aa9def807ea20ee72c582284

--- src/org/lineageos/updater/misc/FileUtils.java
+++ src/org/lineageos/updater/misc/FileUtils.java
@@ -15,7 +15,14 @@
  */
 package org.lineageos.updater.misc;
 
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.ParcelFileDescriptor;
+import android.provider.OpenableColumns;
 import android.util.Log;
+
+import androidx.annotation.NonNull;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -91,5 +98,38 @@ public class FileUtils {
             }
             throw e;
         }
+    }
+
+    public static void copyFile(ContentResolver cr, File sourceFile, Uri destUri,
+                                ProgressCallBack progressCallBack)
+            throws IOException {
+        try (FileChannel sourceChannel = new FileInputStream(sourceFile).getChannel();
+             ParcelFileDescriptor pfd = cr.openFileDescriptor(destUri, "w");
+             FileChannel destChannel = new FileOutputStream(pfd.getFileDescriptor()).getChannel()) {
+            if (progressCallBack != null) {
+                ReadableByteChannel readableByteChannel = new CallbackByteChannel(sourceChannel,
+                        sourceFile.length(), progressCallBack);
+                destChannel.transferFrom(readableByteChannel, 0, sourceChannel.size());
+            } else {
+                destChannel.transferFrom(sourceChannel, 0, sourceChannel.size());
+            }
+        } catch (IOException e) {
+            Log.e(TAG, "Could not copy file", e);
+            /*if (destFile.exists()) {
+                //noinspection ResultOfMethodCallIgnored
+                destFile.delete();
+            }*/
+            throw e;
+        }
+    }
+
+    public static String queryName(@NonNull ContentResolver resolver, Uri uri) {
+        Cursor returnCursor = resolver.query(uri, null, null, null, null);
+        assert returnCursor != null;
+        int nameIndex = returnCursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+        returnCursor.moveToFirst();
+        String name = returnCursor.getString(nameIndex);
+        returnCursor.close();
+        return name;
     }
 }

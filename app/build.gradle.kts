55b918508561b47b78d8d6a39495e6edc70475e7
Sebastiano Barezzi <seba@sebaubuntu.dev>
2023-01-03 01:46:25 +0100

    Updater: Redo project structure
    * Let's be more Android Studio friendly
    * While at it match AOSP deps with Gradle ones and update them
    
    Change-Id: Ib8829e3d954ce43fbf19280d1db2bc43c6fc89e0

---
+++ app/build.gradle.kts
@@ -0,0 +1,80 @@
+import java.util.Properties
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+}
+
+val keystorePropertiesFile = rootProject.file("keystore.properties")
+val keystoreProperties = Properties().apply {
+    if (keystorePropertiesFile.exists()) {
+        load(keystorePropertiesFile.inputStream())
+    }
+}
+
+android {
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "org.lineageos.updater"
+        minSdk = 27
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        getByName("release") {
+            // Includes the default ProGuard rules files.
+            setProguardFiles(
+                listOf(
+                    getDefaultProguardFile("proguard-android-optimize.txt"),
+                    "proguard-rules.pro"
+                )
+            )
+        }
+        getByName("debug") {
+            // Append .dev to package name so we won't conflict with AOSP build.
+            applicationIdSuffix = ".dev"
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    signingConfigs {
+        create("release") {
+            (keystoreProperties["keyAlias"] as String?)?.let {
+                keyAlias = it
+            }
+            (keystoreProperties["keyPassword"] as String?)?.let {
+                keyPassword = it
+            }
+            (keystoreProperties["storeFile"] as String?)?.let {
+                storeFile = file(it)
+            }
+            (keystoreProperties["storePassword"] as String?)?.let {
+                storePassword = it
+            }
+        }
+    }
+}
+
+dependencies {
+    compileOnly(fileTree(mapOf("dir" to "../system_libs", "include" to listOf("*.jar"))))
+
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.5.1")
+    implementation("androidx.cardview:cardview:1.0.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
+    implementation("androidx.localbroadcastmanager:localbroadcastmanager:1.1.0")
+    implementation("androidx.preference:preference:1.2.0")
+    implementation("androidx.recyclerview:recyclerview:1.2.1")
+    implementation("com.google.android.material:material:1.8.0-beta01")
+}

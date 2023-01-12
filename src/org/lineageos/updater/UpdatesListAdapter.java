37d66cd3b8bf60cc19ac7ca1f02d248e5512a30c
althafvly <althafvly@gmail.com>
2023-01-12 18:52:47 +0530

    Updater: Make android studio friendly
    Change-Id: I0047f4689e68b8cb3e98717a0792e4cb854ada57

--- src/org/lineageos/updater/UpdatesListAdapter.java
+++ src/org/lineageos/updater/UpdatesListAdapter.java
@@ -529,7 +529,7 @@ public class UpdatesListAdapter extends RecyclerView.Adapter<UpdatesListAdapter.
         ContextThemeWrapper wrapper = new ContextThemeWrapper(mActivity,
                 R.style.AppTheme_PopupMenuOverlapAnchor);
         PopupMenu popupMenu = new PopupMenu(wrapper, anchor, Gravity.NO_GRAVITY,
-                R.attr.actionOverflowMenuStyle, 0);
+                com.android.internal.R.attr.actionOverflowMenuStyle, 0);
         popupMenu.inflate(R.menu.menu_action_mode);
 
         MenuBuilder menu = (MenuBuilder) popupMenu.getMenu();

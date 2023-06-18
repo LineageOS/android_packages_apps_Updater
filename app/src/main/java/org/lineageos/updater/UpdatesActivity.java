7a2ead755f856f171a5946cb2dbb943a8b15b48c
Michael W <baddaemon87@gmail.com>
2023-06-18 15:47:32 +0200

    Updater: Fix header margin
    * Instead of hardcoding it to 86dp (which was probably fine
      when we didn't have cutouts, calculate the required top margin
      by adding top inset and status bar height and applying it dynamically
    * Increase compile and min sdk versions to avoid warnings - we compile
      using the build sdk (33) when building inline anyway
    
    Change-Id: I80444e5a2247730f06af461a1cce7ac36c6c9631

--- app/src/main/java/org/lineageos/updater/UpdatesActivity.java
+++ app/src/main/java/org/lineageos/updater/UpdatesActivity.java
@@ -33,6 +33,7 @@ import android.os.Bundle;
 import android.os.IBinder;
 import android.os.SystemProperties;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -41,6 +42,7 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.LinearInterpolator;
 import android.view.animation.RotateAnimation;
+import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -51,6 +53,7 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.widget.SwitchCompat;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.WindowInsetsCompat;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import androidx.preference.PreferenceManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -146,6 +149,23 @@ public class UpdatesActivity extends UpdatesListActivity {
             if (actionBar != null) {
                 actionBar.setDisplayShowTitleEnabled(false);
                 actionBar.setDisplayHomeAsUpEnabled(true);
+                int statusBarHeight = 0;
+                TypedValue tv = new TypedValue();
+                if (getTheme().resolveAttribute(android.R.attr.actionBarSize, tv, true)) {
+                    statusBarHeight = TypedValue.complexToDimensionPixelSize(
+                            tv.data, getResources().getDisplayMetrics());
+                }
+                RelativeLayout headerContainer = findViewById(R.id.header_container);
+                final int sbh = statusBarHeight;
+                recyclerView.setOnApplyWindowInsetsListener((view, insets) -> {
+                    int top = insets.getInsets(WindowInsetsCompat.Type.statusBars()).top;
+                    CollapsingToolbarLayout.LayoutParams lp =
+                            (CollapsingToolbarLayout.LayoutParams)
+                                    headerContainer.getLayoutParams();
+                    lp.topMargin = top + sbh;
+                    headerContainer.setLayoutParams(lp);
+                    return insets;
+                });
             }
         }
 

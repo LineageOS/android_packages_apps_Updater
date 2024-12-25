9d8a0ee018684f3b62b3b409c3596b8f62f46689
Nolen Johnson <johnsonnolen@gmail.com>
2024-12-25 00:28:01 -0500

    Updater: Allow Updater to handle major upgrades via property
    * If the user has GMS installed, they'll need to install an updated
      copy, as addon.d won't restore it, and if it did it likely wouldn't
      work. So we can't support this unconditionally.
    * If the user has no-GMS, this should be totally fine to do, so the
      user could set the property, upgrade, and go on their merry way.
      This use-case is why I didn't make this an `ro.` property.
    * If someone building LineageOS builds GMS into the images, upgrade
      via this method would also be feasible.
    
    Change-Id: If2a6e6767dcb182d3dbd2a95c81a940fdb53547e

--- app/src/main/java/org/lineageos/updater/misc/Constants.java
+++ app/src/main/java/org/lineageos/updater/misc/Constants.java
@@ -39,6 +39,7 @@ public final class Constants {
     public static final String UNCRYPT_FILE_EXT = ".uncrypt";
 
     public static final String PROP_AB_DEVICE = "ro.build.ab_update";
+    public static final String PROP_ALLOW_MAJOR_UPGRADES = "lineage.updater.allow_major_upgrades";
     public static final String PROP_BUILD_DATE = "ro.build.date.utc";
     public static final String PROP_BUILD_VERSION = "ro.lineage.build.version";
     public static final String PROP_BUILD_VERSION_INCREMENTAL = "ro.build.version.incremental";

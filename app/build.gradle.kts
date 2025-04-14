5540fc34adff0a6514f9a331234e1de526587859
pjgowtham <pjgowtham@gmail.com>
2025-04-14 19:45:50 +0530

    Updater: gradle updates and use java 17
    Change-Id: Iaa705eb711543a27a440c834a97260b62461115e

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,14 +30,18 @@ val keystoreProperties = Properties().apply {
 }
 
 android {
-    compileSdk = 33
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "org.lineageos.updater"
         minSdk = 32
-        targetSdk = 33
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
+    }
+
+    buildFeatures {
+        buildConfig = true
     }
 
     buildTypes {
@@ -57,12 +61,12 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
 
     signingConfigs {

6aa7fb4476db48bb9a7b69ae44f8493eb04354ba
pjgowtham <pjgowtham@gmail.com>
2025-04-14 22:48:02 +0530

    Updater: Remove config_hideRecoveryUpdate and "Update recovery" on AB
    Devices with prebuilt vendor are not taken into account because
    nobody does that anymore. The "Update recovery" menu preference
    visibility is now opt-in and is meant only for the deprecated
    Non-AB OTA methodology.
    
    The same menu is now not visible in AB devices and thus the toast
    message is also removed because the users are now fairly adapted to
    the recovery being updated and slightly better UX to avoid a greyed
    out preference button.
    
    Change-Id: I0973de60530d0800a93e2b107996d78cffd8649e

--- app/src/main/java/org/lineageos/updater/UpdatesActivity.java
+++ app/src/main/java/org/lineageos/updater/UpdatesActivity.java
@@ -41,12 +41,10 @@ import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
@@ -546,32 +544,11 @@ public class UpdatesActivity extends UpdatesListActivity implements UpdateImport
                 prefs.getBoolean(Constants.PREF_MOBILE_DATA_WARNING, true)));
         abPerfMode.setChecked(prefs.getBoolean(Constants.PREF_AB_PERF_MODE, false));
 
-        if (getResources().getBoolean(R.bool.config_hideRecoveryUpdate)) {
-            // Hide the update feature if explicitly requested.
-            // Might be the case of A-only devices using prebuilt vendor images.
-            updateRecovery.setVisibility(View.GONE);
-        } else if (Utils.isRecoveryUpdateExecPresent()) {
+        if (Utils.isRecoveryUpdateExecPresent()) {
+            updateRecovery.setVisibility(View.VISIBLE);
+            // Obtain and apply the user preference from SetupWizard.
             updateRecovery.setChecked(
                     SystemProperties.getBoolean(Constants.UPDATE_RECOVERY_PROPERTY, false));
-        } else {
-            // There is no recovery updater script in the device, so the feature is considered
-            // forcefully enabled, just to avoid users to be confused and complain that
-            // recovery gets overwritten. That's the case of A/B and recovery-in-boot devices.
-            updateRecovery.setChecked(true);
-            updateRecovery.setOnTouchListener(new View.OnTouchListener() {
-                private Toast forcedUpdateToast = null;
-
-                @Override
-                public boolean onTouch(View v, MotionEvent event) {
-                    if (forcedUpdateToast != null) {
-                        forcedUpdateToast.cancel();
-                    }
-                    forcedUpdateToast = Toast.makeText(getApplicationContext(),
-                            getString(R.string.toast_forced_update_recovery), Toast.LENGTH_SHORT);
-                    forcedUpdateToast.show();
-                    return true;
-                }
-            });
         }
 
         new AlertDialog.Builder(this)

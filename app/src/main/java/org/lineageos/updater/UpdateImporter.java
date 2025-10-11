d64a7399e29ae25b87cc19a03cb4a9d1b58bef4a
pjgowtham <pjgowtham@gmail.com>
2025-10-11 08:24:38 +0530

    Updater: Fix duplicated and truncated date for local updates
    When importing a local update, the date was appended to the
    version string. This caused the date to appear twice in the
    UI—once in the title and again on the line below.
    The change removes the appended date so that truncated text
    is not shown.
    
    Change-Id: Ifd206242be8b7485a3845703f17bb3f285a519fd

--- app/src/main/java/org/lineageos/updater/UpdateImporter.java
+++ app/src/main/java/org/lineageos/updater/UpdateImporter.java
@@ -24,7 +24,6 @@ import android.os.ParcelFileDescriptor;
 import android.util.Log;
 
 import org.lineageos.updater.controller.UpdaterController;
-import org.lineageos.updater.misc.StringGenerator;
 import org.lineageos.updater.misc.Utils;
 import org.lineageos.updater.model.Update;
 import org.lineageos.updater.model.UpdateStatus;
@@ -36,7 +35,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
-import java.text.DateFormat;
 import java.util.Enumeration;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
@@ -143,8 +141,6 @@ public class UpdateImporter {
 
     private Update buildLocalUpdate(File file) {
         final long timeStamp = getTimeStamp(file);
-        final String buildDate = StringGenerator.getDateLocalizedUTC(
-                activity, DateFormat.MEDIUM, timeStamp);
         final String name = activity.getString(R.string.local_update_name);
         final Update update = new Update();
         update.setAvailableOnline(false);
@@ -155,7 +151,7 @@ public class UpdateImporter {
         update.setTimestamp(timeStamp);
         update.setStatus(UpdateStatus.VERIFIED);
         update.setPersistentStatus(UpdateStatus.Persistent.VERIFIED);
-        update.setVersion(String.format("%s (%s)", name, buildDate));
+        update.setVersion(name);
         return update;
     }
 

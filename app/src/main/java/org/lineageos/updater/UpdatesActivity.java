4fb3d8126e34a1dcb952ce0413cd4d67869a36b0
pjgowtham <pjgowtham@gmail.com>
2025-10-19 22:48:57 +0530

    Updater: Replace AlarmManager with WorkManager
    Change-Id: Ia51eadb42b324943f611a5ee0c133314bdc37eff

--- app/src/main/java/org/lineageos/updater/UpdatesActivity.java
+++ app/src/main/java/org/lineageos/updater/UpdatesActivity.java
@@ -424,10 +424,10 @@ public class UpdatesActivity extends UpdatesListActivity implements UpdateImport
             updateLastCheckedString();
             if (json.exists() && Utils.isUpdateCheckEnabled(this) &&
                     Utils.checkForNewUpdates(json, jsonNew)) {
-                UpdatesCheckReceiver.updateRepeatingUpdatesCheck(this);
+                UpdatesCheckWorker.schedule(this);
             }
             // In case we set a one-shot check because of a previous failure
-            UpdatesCheckReceiver.cancelUpdatesCheck(this);
+            UpdatesCheckWorker.cancel(this);
             //noinspection ResultOfMethodCallIgnored
             jsonNew.renameTo(json);
         } catch (IOException | JSONException e) {
@@ -580,10 +580,9 @@ public class UpdatesActivity extends UpdatesListActivity implements UpdateImport
                             .apply();
 
                     if (Utils.isUpdateCheckEnabled(this)) {
-                        UpdatesCheckReceiver.scheduleRepeatingUpdatesCheck(this);
+                        UpdatesCheckWorker.schedule(this);
                     } else {
-                        UpdatesCheckReceiver.cancelRepeatingUpdatesCheck(this);
-                        UpdatesCheckReceiver.cancelUpdatesCheck(this);
+                        UpdatesCheckWorker.cancel(this);
                     }
 
                     if (Utils.isABDevice()) {

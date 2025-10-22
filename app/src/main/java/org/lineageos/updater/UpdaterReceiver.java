c242317e743493393ce0b2f5abde4eba01723a8e
pjgowtham <pjgowtham@gmail.com>
2025-10-22 23:42:55 +0530

    Updater: Deprecate BuildInfoUtils.Java
    Change-Id: I12933303bbc6692d512ecdadd059f82ac3a6d898

--- app/src/main/java/org/lineageos/updater/UpdaterReceiver.java
+++ app/src/main/java/org/lineageos/updater/UpdaterReceiver.java
@@ -29,7 +29,6 @@ import androidx.core.app.NotificationCompat;
 import androidx.preference.PreferenceManager;
 
 import org.lineageos.updater.controller.UpdaterService;
-import org.lineageos.updater.misc.BuildInfoUtils;
 import org.lineageos.updater.misc.Constants;
 import org.lineageos.updater.misc.StringGenerator;
 
@@ -62,7 +61,7 @@ public class UpdaterReceiver extends BroadcastReceiver {
         String buildDate = StringGenerator.getDateLocalizedUTC(context,
                 DateFormat.MEDIUM, preferences.getLong(Constants.PREF_INSTALL_NEW_TIMESTAMP, 0));
         String buildInfo = context.getString(R.string.list_build_version_date,
-                BuildInfoUtils.getBuildVersion(), buildDate);
+                Utils.getBuildVersion(), buildDate);
 
         Intent notificationIntent = new Intent(context, UpdatesActivity.class);
         PendingIntent intent = PendingIntent.getActivity(context, 0, notificationIntent,

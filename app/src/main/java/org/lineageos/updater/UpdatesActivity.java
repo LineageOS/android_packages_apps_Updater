2f4aa55ce676c0ec03253655d8b62ad394fd8ba3
pjgowtham <pjgowtham@gmail.com>
2025-10-26 22:17:21 +0530

    Updater: Automatically check for updates when connectivity is restored
    The user might enter updater when there is no connectivity.
    Thus this ensures that a callback is established to check
    for update whenever feasible.
    
    Change-Id: I00443e80f5920220a058c7d0fc4338745db9a2a9

--- app/src/main/java/org/lineageos/updater/UpdatesActivity.java
+++ app/src/main/java/org/lineageos/updater/UpdatesActivity.java
@@ -27,6 +27,10 @@ import android.content.ServiceConnection;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.icu.text.DateFormat;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
+import android.net.NetworkRequest;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -46,6 +50,7 @@ import android.widget.Toast;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AlertDialog;
@@ -84,6 +89,7 @@ public class UpdatesActivity extends UpdatesListActivity implements UpdateImport
     private static final String TAG = "UpdatesActivity";
     private UpdaterService mUpdaterService;
     private BroadcastReceiver mBroadcastReceiver;
+    private ConnectivityManager.NetworkCallback mNetworkCallback;
 
     private UpdatesListAdapter mAdapter;
 
@@ -253,6 +259,11 @@ public class UpdatesActivity extends UpdatesListActivity implements UpdateImport
         LocalBroadcastManager.getInstance(this).unregisterReceiver(mBroadcastReceiver);
         if (mUpdaterService != null) {
             unbindService(mConnection);
+        }
+        if (mNetworkCallback != null) {
+            ConnectivityManager cm = getSystemService(ConnectivityManager.class);
+            cm.unregisterNetworkCallback(mNetworkCallback);
+            mNetworkCallback = null;
         }
         super.onStop();
     }
@@ -441,6 +452,7 @@ public class UpdatesActivity extends UpdatesListActivity implements UpdateImport
                     if (!cancelled) {
                         showSnackbar(R.string.snack_updates_check_failed, Snackbar.LENGTH_LONG);
                     }
+                    registerNetworkCallback();
                 });
             }
 
@@ -623,5 +635,24 @@ public class UpdatesActivity extends UpdatesListActivity implements UpdateImport
                         .putBoolean(Constants.HAS_SEEN_WELCOME_MESSAGE, true)
                         .apply())
                 .show();
+    }
+
+    private void registerNetworkCallback() {
+        if (mNetworkCallback != null) {
+            Log.e(TAG, "Network callback already registered. Skipping.");
+            return;
+        }
+        ConnectivityManager cm = getSystemService(ConnectivityManager.class);
+        NetworkRequest request = new NetworkRequest.Builder()
+                .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+                .addCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
+                .build();
+        mNetworkCallback = new ConnectivityManager.NetworkCallback() {
+            @Override
+            public void onAvailable(@NonNull Network network) {
+                runOnUiThread(() -> downloadUpdatesList());
+            }
+        };
+        cm.registerNetworkCallback(request, mNetworkCallback);
     }
 }

3a006b341f1fffedc18ae9f5de6ccddc4efb2c7a
pjgowtham <pjgowtham@gmail.com>
2026-01-30 13:58:51 +0530

    Updater: Update generatebp to v1.31
    Change-Id: Ia6ec63eb08e412fe138dca25eaf915a5a83d4e69

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,7 +14,7 @@ pluginManagement {
         }
         mavenCentral()
         gradlePluginPortal()
-        maven("https://raw.githubusercontent.com/lineage-next/gradle-generatebp/v1.20/.m2")
+        maven("https://raw.githubusercontent.com/lineage-next/gradle-generatebp/v1.31/.m2")
     }
 }
 

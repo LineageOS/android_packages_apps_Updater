b74a4acaa2e2010079d5a18d0dc1a61e4523377e
pjgowtham <pjgowtham@gmail.com>
2026-01-30 13:38:51 +0530

    Updater: Migrate generatebp to plugin declaration
    Change-Id: Ie573c5cb09bce1a5a36c6e268e5b1bc535297de7

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,11 +5,19 @@
 
 pluginManagement {
     repositories {
-        gradlePluginPortal()
-        google()
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
         mavenCentral()
+        gradlePluginPortal()
+        maven("https://raw.githubusercontent.com/lineage-next/gradle-generatebp/v1.20/.m2")
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
@@ -17,5 +25,6 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
+
 rootProject.name = "Updater"
 include(":app")

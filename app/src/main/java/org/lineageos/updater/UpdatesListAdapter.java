8d3d9e2b6545f4bfbcd542c1aaeed8185ecf78e1
pjgowtham <pjgowtham@gmail.com>
2026-03-08 23:15:52 +0530

    Updater: Use SettingsLib SettingsOpacity for disabled alpha
    Change-Id: I074be755e9243b82d3fff7cc3403fa170bd06961

--- app/src/main/java/org/lineageos/updater/UpdatesListAdapter.java
+++ app/src/main/java/org/lineageos/updater/UpdatesListAdapter.java
@@ -15,7 +15,6 @@ import android.text.format.Formatter;
 import android.text.method.LinkMovementMethod;
 import android.text.util.Linkify;
 import android.util.Log;
-import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -34,6 +33,8 @@ import androidx.appcompat.view.menu.MenuPopupHelper;
 import androidx.appcompat.widget.PopupMenu;
 import androidx.preference.PreferenceManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.settingslib.spa.framework.theme.SettingsOpacity;
 
 import com.google.android.material.snackbar.Snackbar;
 
@@ -61,8 +62,6 @@ public class UpdatesListAdapter extends RecyclerView.Adapter<UpdatesListAdapter.
     private static final int BATTERY_PLUGGED_ANY = BatteryManager.BATTERY_PLUGGED_AC
             | BatteryManager.BATTERY_PLUGGED_USB
             | BatteryManager.BATTERY_PLUGGED_WIRELESS;
-
-    private final float mAlphaDisabledValue;
 
     private List<String> mDownloadIds;
     private String mSelectedDownload;
@@ -113,10 +112,6 @@ public class UpdatesListAdapter extends RecyclerView.Adapter<UpdatesListAdapter.
 
     public UpdatesListAdapter(UpdatesListActivity activity) {
         mActivity = activity;
-
-        TypedValue tv = new TypedValue();
-        mActivity.getTheme().resolveAttribute(android.R.attr.disabledAlpha, tv, true);
-        mAlphaDisabledValue = tv.getFloat();
     }
 
     @NonNull
@@ -407,7 +402,7 @@ public class UpdatesListAdapter extends RecyclerView.Adapter<UpdatesListAdapter.
             default:
                 clickListener = null;
         }
-        button.setAlpha(enabled ? 1.f : mAlphaDisabledValue);
+        button.setAlpha(enabled ? 1.f : SettingsOpacity.Disabled);
 
         // Disable action mode when a button is clicked
         button.setOnClickListener(v -> {

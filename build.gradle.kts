b770aef821e87b720542b5d657d91c6968576e39
pjgowtham <pjgowtham@gmail.com>
2026-03-16 14:07:34 +0530

    Updater: Add Ktor dependencies
    Change-Id: I0613470f34f087422beed1f4ffb6291ce2b5c63e

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.lineageos.generatebp) apply false
 }
 

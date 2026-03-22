2649a6cd22ebbcf911e2ad7a693e5537205e127e
pjgowtham <pjgowtham@gmail.com>
2026-03-23 00:30:10 +0530

    Updater: Move preferences entry and local update prefs to footer
    Change-Id: Iff73a84cf806f206b33013b2e5d4a4ea1aabf3ba

--- app/src/main/java/org/lineageos/updater/UpdatesScaffoldActivity.kt
+++ app/src/main/java/org/lineageos/updater/UpdatesScaffoldActivity.kt
@@ -12,8 +12,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.CompositionLocalProvider
@@ -30,8 +34,11 @@ import androidx.compose.ui.viewinterop.AndroidView
 import com.android.settingslib.spa.framework.compose.LocalNavController
 import com.android.settingslib.spa.framework.compose.NavControllerWrapper
 import com.android.settingslib.spa.framework.theme.SettingsTheme
+import com.android.settingslib.spa.widget.preference.Preference
+import com.android.settingslib.spa.widget.preference.PreferenceModel
 import com.android.settingslib.spa.widget.scaffold.MoreOptionsAction
 import com.android.settingslib.spa.widget.scaffold.SettingsScaffold
+import com.android.settingslib.spa.widget.ui.Category
 import org.lineageos.updater.data.UpdateStatus
 import org.lineageos.updater.preferences.PreferencesActivity
 
@@ -94,30 +101,49 @@ abstract class UpdatesScaffoldActivity : ComponentActivity() {
                             }
 
                             MoreOptionsAction {
-                                MenuItem(stringResource(R.string.local_update_import)) {
-                                    onLocalUpdateClick()
-                                }
-                                MenuItem(stringResource(R.string.menu_preferences)) {
-                                    startActivity(
-                                        Intent(
-                                            this@UpdatesScaffoldActivity,
-                                            PreferencesActivity::class.java,
-                                        )
-                                    )
-                                }
                                 MenuItem(stringResource(R.string.menu_show_changelog)) {
                                     onChangelogClick()
                                 }
                             }
                         },
                     ) { paddingValues ->
-                        AndroidView(
-                            factory = { capturedView },
-                            modifier = Modifier
+                        val localUpdateSummary =
+                            stringResource(R.string.local_update_import_summary)
+                        val preferencesSummary =
+                            stringResource(R.string.preferences_summary)
+                        Column(
+                            Modifier
                                 .fillMaxSize()
                                 .padding(paddingValues)
-                                .nestedScroll(rememberNestedScrollInteropConnection()),
-                        )
+                                .verticalScroll(rememberScrollState())
+                        ) {
+                            AndroidView(
+                                factory = { capturedView },
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .nestedScroll(rememberNestedScrollInteropConnection()),
+                            )
+
+                            Category {
+                                Preference(object : PreferenceModel {
+                                    override val title = stringResource(R.string.local_update_import)
+                                    override val summary = { localUpdateSummary }
+                                    override val onClick = { onLocalUpdateClick() }
+                                })
+                                Preference(object : PreferenceModel {
+                                    override val title = stringResource(R.string.menu_preferences)
+                                    override val summary = { preferencesSummary }
+                                    override val onClick = {
+                                        startActivity(
+                                            Intent(
+                                                this@UpdatesScaffoldActivity,
+                                                PreferencesActivity::class.java,
+                                            )
+                                        )
+                                    }
+                                })
+                            }
+                        }
                     }
                 }
             }

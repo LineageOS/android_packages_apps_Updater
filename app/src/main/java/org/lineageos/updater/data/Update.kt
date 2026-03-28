80e6da40e77ef744cce75176c5b3f09e76e1be65
pjgowtham <pjgowtham@gmail.com>
2026-03-28 21:09:26 +0530

    Updater: Enforce compile-time database correctness
    Migrate to Room.
    
    Change-Id: I7a2c0c24e7c045f6e78a3742449773c97c858207

--- app/src/main/java/org/lineageos/updater/data/Update.kt
+++ app/src/main/java/org/lineageos/updater/data/Update.kt
@@ -16,13 +16,13 @@ data class Update(
     val fileSize: Long = 0,
     val isFinalizing: Boolean = false,
     val installProgress: Int = 0,
-    val name: String? = null,
+    val name: String = "",
     val progress: Int = 0,
     val speed: Long = 0,
     val status: UpdateStatus = UpdateStatus.UNKNOWN,
     val timestamp: Long = 0,
     val type: String? = null,
-    val version: String? = null,
+    val version: String = "",
 ) {
     fun withAvailableOnline(v: Boolean) = copy(isAvailableOnline = v)
     fun withDownloadId(v: String) = copy(downloadId = v)
@@ -32,13 +32,13 @@ data class Update(
     fun withFileSize(v: Long) = copy(fileSize = v)
     fun withFinalizing(v: Boolean) = copy(isFinalizing = v)
     fun withInstallProgress(v: Int) = copy(installProgress = v)
-    fun withName(v: String?) = copy(name = v)
+    fun withName(v: String) = copy(name = v)
     fun withProgress(v: Int) = copy(progress = v)
     fun withSpeed(v: Long) = copy(speed = v)
     fun withStatus(v: UpdateStatus) = copy(status = v)
     fun withTimestamp(v: Long) = copy(timestamp = v)
     fun withType(v: String?) = copy(type = v)
-    fun withVersion(v: String?) = copy(version = v)
+    fun withVersion(v: String) = copy(version = v)
 
     fun toBuilder() = Builder(this)
 
@@ -51,13 +51,13 @@ data class Update(
         private var fileSize: Long = 0,
         private var isFinalizing: Boolean = false,
         private var installProgress: Int = 0,
-        private var name: String? = null,
+        private var name: String = "",
         private var progress: Int = 0,
         private var speed: Long = 0,
         private var status: UpdateStatus = UpdateStatus.UNKNOWN,
         private var timestamp: Long = 0,
         private var type: String? = null,
-        private var version: String? = null,
+        private var version: String = "",
     ) {
         constructor(update: Update) : this(
             update.isAvailableOnline, update.downloadId, update.downloadUrl,
@@ -74,13 +74,13 @@ data class Update(
         fun setFileSize(v: Long) = apply { fileSize = v }
         fun setFinalizing(v: Boolean) = apply { isFinalizing = v }
         fun setInstallProgress(v: Int) = apply { installProgress = v }
-        fun setName(v: String?) = apply { name = v }
+        fun setName(v: String) = apply { name = v }
         fun setProgress(v: Int) = apply { progress = v }
         fun setSpeed(v: Long) = apply { speed = v }
         fun setStatus(v: UpdateStatus) = apply { status = v }
         fun setTimestamp(v: Long) = apply { timestamp = v }
         fun setType(v: String?) = apply { type = v }
-        fun setVersion(v: String?) = apply { version = v }
+        fun setVersion(v: String) = apply { version = v }
         fun build() = Update(
             isAvailableOnline, downloadId, downloadUrl, eta, file, fileSize,
             isFinalizing, installProgress, name, progress, speed, status,

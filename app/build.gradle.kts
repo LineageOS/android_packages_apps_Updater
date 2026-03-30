0ce3694dd3b48b2ad2a33442f12790cd82f7342f
pjgowtham <pjgowtham@gmail.com>
2026-03-30 21:27:00 +0530

    Updater: Add material 3 extended icons dependency
    These are already transitively pulled in AOSP build process
    and are only added for the purpose of gradle builds.
    
    Change-Id: I774f8180b8d62a69b3c29f8c7c70cfe7c3c5aa1d

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -98,6 +98,7 @@ dependencies {
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.cardview)
+    implementation(libs.androidx.compose.material.icons.extended)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.runtime)
     implementation(libs.androidx.compose.ui.tooling.preview)

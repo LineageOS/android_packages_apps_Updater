e92494971012b832cd36ffa7d47927129aa2eedc
pjgowtham <pjgowtham@gmail.com>
2026-05-01 14:03:00 +0530

    Updater: Improve Utils.canInstall and migrate to InstallUtils
    An update can fail pre-flight check for downgrades and major
    upgrades. This change ensures that the UI can represent the
    failure better.
    
    Change-Id: I94b0b8d0fbd72c6e1c54c338a5b9f2af640568a7

--- app/src/main/java/org/lineageos/updater/util/InstallUtils.kt
+++ app/src/main/java/org/lineageos/updater/util/InstallUtils.kt
@@ -5,6 +5,9 @@
 
 package org.lineageos.updater.util
 
+import org.lineageos.updater.data.Update
+import org.lineageos.updater.deviceinfo.DeviceInfoUtils
+import org.lineageos.updater.misc.Utils
 import java.io.File
 
 object InstallUtils {
@@ -14,4 +17,24 @@ object InstallUtils {
             lines.any { it.split(" ")[1] == "/mnt/scratch" }
         }
     }.getOrDefault(false)
+
+    enum class BlockedReason {
+        NONE, DOWNGRADE, VERSION_UNSUPPORTED
+    }
+
+    @JvmStatic
+    fun getBlockedReason(update: Update) = when {
+        !DeviceInfoUtils.isDowngradingAllowed
+                && update.timestamp <= DeviceInfoUtils.buildDateTimestamp
+            -> BlockedReason.DOWNGRADE
+
+        !Utils.compareVersions(
+            update.version, DeviceInfoUtils.buildVersion, DeviceInfoUtils.isMajorUpdateAllowed
+        ) -> BlockedReason.VERSION_UNSUPPORTED
+
+        else -> BlockedReason.NONE
+    }
+
+    @JvmStatic
+    fun canInstall(update: Update) = getBlockedReason(update) == BlockedReason.NONE
 }

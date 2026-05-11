4965e2cdd14bac97a6da653aab18613912f3325f
pjgowtham <pjgowtham@gmail.com>
2026-05-11 17:51:33 +0530

    Updater: Wire up preferences screen and migrate settings access
    The updater is injected into the system Settings app, so launching
    preferences as a separate Activity gives the standard back-stack
    navigation animation consistent with the rest of Settings, rather
    than an in-app transition that would look out of place.
    
    The old user preferences are not migrated since its adds
    complexity for something minor.
    
    The default behaviour is also modified :
    
    Auto-delete updates is enabled by default.
    AB performance mode toggle is respected even when an update is in progress.
    
    Change-Id: I1235bc7f187403c3798ea553c01d1f7d16d0aa25

--- app/src/main/java/org/lineageos/updater/controller/ABUpdateInstaller.java
+++ app/src/main/java/org/lineageos/updater/controller/ABUpdateInstaller.java
@@ -17,6 +17,7 @@ import androidx.preference.PreferenceManager;
 import org.lineageos.updater.UpdaterApplication;
 import org.lineageos.updater.data.Update;
 import org.lineageos.updater.data.UpdateStatus;
+import org.lineageos.updater.data.UserPreferencesRepository;
 import org.lineageos.updater.misc.Constants;
 import org.lineageos.updater.misc.Utils;
 
@@ -224,8 +225,7 @@ class ABUpdateInstaller {
             }
         }
 
-        applyPerformanceMode(PreferenceManager.getDefaultSharedPreferences(mContext)
-                .getBoolean(Constants.PREF_AB_PERF_MODE, false));
+        applyPerformanceMode(UserPreferencesRepository.getAbPerfModeBlocking(mContext));
 
         String zipFileUri = "file://" + file.getAbsolutePath();
         try {
@@ -273,8 +273,7 @@ class ABUpdateInstaller {
             return;
         }
 
-        applyPerformanceMode(PreferenceManager.getDefaultSharedPreferences(mContext)
-                .getBoolean(Constants.PREF_AB_PERF_MODE, false));
+        applyPerformanceMode(UserPreferencesRepository.getAbPerfModeBlocking(mContext));
     }
 
     private void installationDone(boolean needsReboot) {
@@ -304,10 +303,6 @@ class ABUpdateInstaller {
                 update.withStatus(UpdateStatus.INSTALLATION_CANCELLED));
         mUpdaterController.notifyUpdateChange(mDownloadId);
 
-    }
-
-    public void setPerformanceMode(boolean enable) {
-        applyPerformanceMode(enable);
     }
 
     public void suspend() {

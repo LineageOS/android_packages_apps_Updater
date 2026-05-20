7dfb080da85a8d530c88c195e3d8f8e837c849ed
LuK1337 <priv.luk@gmail.com>
2026-05-20 11:44:33 +0200

    Updater: Migrate to api/v2
    Change-Id: I08c2bb0fe7e8f217bab721665488aef44829cc36

--- app/src/main/java/org/lineageos/updater/data/source/network/NetworkUpdate.kt
+++ app/src/main/java/org/lineageos/updater/data/source/network/NetworkUpdate.kt
@@ -5,36 +5,53 @@
 
 package org.lineageos.updater.data.source.network
 
+import kotlinx.serialization.json.JsonIgnoreUnknownKeys
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import org.lineageos.updater.data.Update
 
 @Suppress("PROVIDED_RUNTIME_TOO_LOW")
 @Serializable
+@OptIn(ExperimentalSerializationApi::class)
+@JsonIgnoreUnknownKeys
 data class NetworkUpdate(
-    @SerialName("datetime") val timestamp: Long,
-    @SerialName("filename") val name: String,
-    @SerialName("id") val downloadId: String,
-    @SerialName("romtype") val type: String,
-    @SerialName("size") val fileSize: Long,
-    @SerialName("url") val downloadUrl: String,
+    @SerialName("date") val date: String,
+    @SerialName("datetime") val datetime: Long,
+    @SerialName("files") val files: List<NetworkUpdateFile>,
+    @SerialName("os_patch_level") val osPatchLevel: String? = null,
+    @SerialName("os_sdk_level") val osSdkLevel: Int? = null,
+    @SerialName("type") val type: String,
     @SerialName("version") val version: String,
 )
 
 @Suppress("PROVIDED_RUNTIME_TOO_LOW")
 @Serializable
-data class NetworkUpdateResponse(
-    @SerialName("response") val updates: List<NetworkUpdate>,
+@OptIn(ExperimentalSerializationApi::class)
+@JsonIgnoreUnknownKeys
+data class NetworkUpdateFile(
+    @SerialName("date") val date: String? = null,
+    @SerialName("datetime") val datetime: Long? = null,
+    @SerialName("filename") val filename: String,
+    @SerialName("filepath") val filepath: String,
+    @SerialName("os_patch_level") val osPatchLevel: String? = null,
+    @SerialName("os_sdk_level") val osSdkLevel: Int? = null,
+    @SerialName("sha1") val sha1: String,
+    @SerialName("sha256") val sha256: String,
+    @SerialName("size") val size: Long,
+    @SerialName("type") val type: String? = null,
+    @SerialName("url") val url: String,
 )
 
+@OptIn(ExperimentalSerializationApi::class)
 fun NetworkUpdate.toUpdate(): Update {
     return Update(
-        downloadId = downloadId,
-        name = name,
-        timestamp = timestamp,
+        downloadId = files[0].sha256,
+        name = files[0].filename,
+        timestamp = datetime,
         type = type,
-        fileSize = fileSize,
-        downloadUrl = downloadUrl,
+        fileSize = files[0].size,
+        downloadUrl = files[0].url,
         version = version,
         isAvailableOnline = true,
     )

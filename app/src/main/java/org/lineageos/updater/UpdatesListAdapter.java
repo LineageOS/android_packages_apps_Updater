e8588373c5b751f45add0492c1860e0403c7f857
pjgowtham <pjgowtham@gmail.com>
2026-05-21 07:16:14 +0530

    Updater: Drop welcome and info dialogs
    Both dialogs exist solely to inform users that updates are full
    packages and can be installed without going update-by-update. Since
    all served updates are full packages and delta packages are not used,
    this information is redundant. Strings are retained at the bottom of
    strings.xml for reference.
    
    While the above information is not for layman users, the future
    UI updates to lists will ensure that the latest update is highlighted
    or even a default behavior to show only the latest update if need be.
    
    Change-Id: Icea8fa9e80c0ab2ac8abc9a4efe39c38a55f86eb

--- app/src/main/java/org/lineageos/updater/UpdatesListAdapter.java
+++ app/src/main/java/org/lineageos/updater/UpdatesListAdapter.java
@@ -539,7 +539,6 @@ public class UpdatesListAdapter extends RecyclerView.Adapter<UpdatesListAdapter.
                 .setPositiveButton(android.R.string.ok,
                         (dialog, which) -> {
                             Utils.triggerUpdate(mActivity, downloadId);
-                            maybeShowInfoDialog();
                         })
                 .setNegativeButton(android.R.string.cancel, null);
     }
@@ -555,21 +554,6 @@ public class UpdatesListAdapter extends RecyclerView.Adapter<UpdatesListAdapter.
                             mActivity.startService(intent);
                         })
                 .setNegativeButton(android.R.string.cancel, null);
-    }
-
-    private void maybeShowInfoDialog() {
-        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(mActivity);
-        boolean alreadySeen = preferences.getBoolean(Constants.PREF_HAS_SEEN_INFO_DIALOG, false);
-        if (alreadySeen) {
-            return;
-        }
-        new AlertDialog.Builder(mActivity)
-                .setTitle(R.string.info_dialog_title)
-                .setMessage(R.string.info_dialog_message)
-                .setPositiveButton(R.string.info_dialog_ok, (dialog, which) -> preferences.edit()
-                        .putBoolean(Constants.PREF_HAS_SEEN_INFO_DIALOG, true)
-                        .apply())
-                .show();
     }
 
     private void startActionMode(final Update update, final boolean canDelete, View anchor) {

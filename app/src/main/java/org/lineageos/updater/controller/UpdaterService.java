7d5f2fd3a586ff3cbb9c5bce3704d492419ea82d
pjgowtham <pjgowtham@gmail.com>
2026-06-14 15:27:46 +0530

    Updater: Stream A/B updates by default
    Use ota_property_files from the server API response to stream A/B
    updates directly through UpdateEngine. Streaming avoids storing the full
    OTA before installation and remains suspendable, so make it the default
    for online A/B updates. Users who need an exportable package can disable
    streaming and follow the download-install-export-delete flow.
    
    Keep payload_metadata offset and size stored for future use; they are
    not consumed yet.
    
    Unlike the regular package downloader, streaming does not follow
    duplicate links. After the payload URL is handed to UpdateEngine,
    theupdater cannot consistently switch mirrors.
    
    Change-Id: I436693313c498fb981083f6d8e0ab280639cd0e7

--- app/src/main/java/org/lineageos/updater/controller/UpdaterService.java
+++ app/src/main/java/org/lineageos/updater/controller/UpdaterService.java
@@ -28,9 +28,10 @@ import org.lineageos.updater.UpdatesActivity;
 import org.lineageos.updater.data.Update;
 import org.lineageos.updater.data.UpdateStatus;
 import org.lineageos.updater.data.UserPreferencesRepository;
-import org.lineageos.updater.util.StringUtil;
 import org.lineageos.updater.misc.Utils;
 import org.lineageos.updater.notifications.NotificationHelper;
+import org.lineageos.updater.util.InstallUtils;
+import org.lineageos.updater.util.StringUtil;
 
 import java.io.IOException;
 import java.time.format.FormatStyle;
@@ -187,14 +188,17 @@ public class UpdaterService extends Service {
                 Log.e(TAG, "Update not found: " + downloadId);
                 return START_NOT_STICKY;
             }
-            if (!update.getStatus().hasVerifiedPackage()) {
-                throw new IllegalArgumentException(update.getDownloadId() + " is not verified");
-            }
+            boolean canStreamUpdate = InstallUtils.canStreamUpdate(update,
+                    mUserPreferencesRepository.getStreamUpdatesBlocking());
             try {
-                if (Utils.isABUpdate(update.getFile())) {
+                if (canStreamUpdate || Utils.isABUpdate(update.getFile())) {
                     ABUpdateInstaller installer = ABUpdateInstaller.getInstance(this,
                             mUpdaterController, mUserPreferencesRepository);
-                    installer.install(downloadId);
+                    if (canStreamUpdate) {
+                        installer.installStreaming(downloadId);
+                    } else {
+                        installer.install(downloadId);
+                    }
                 } else {
                     UpdateInstaller installer = UpdateInstaller.getInstance(this,
                             mUpdaterController);

979cb16367b3fbfb6b26434240216c88fa15671a
LuK1337 <priv.luk@gmail.com>
2026-07-15 23:51:43 +0200

    Updater: push-update: Use accurate timestamp
    Change-Id: I719cd5c4ffcb8105b2c4462b1ff3ecd1ba487387

--- push-update.sh
+++ push-update.sh
@@ -49,14 +49,7 @@ zip_name=`basename "$zip_path"`
 id=`echo "$zip_name" | sha1sum | cut -d' ' -f1`
 version=`echo "$zip_name" | cut -d'-' -f2`
 type=`echo "$zip_name" | cut -d'-' -f4`
-build_date=`echo "$zip_name" | cut -d'-' -f3 | cut -d'_' -f1`
-if [ "`uname`" = "Darwin" ]; then
-    timestamp=`date -jf "%Y%m%d %H:%M:%S" "$build_date 23:59:59" +%s`
-    size=`stat -f%z "$zip_path"`
-else
-    timestamp=`date --date="$build_date 23:59:59" +%s`
-    size=`stat -c "%s" "$zip_path"`
-fi
+timestamp=`unzip -p "$zip_path" META-INF/com/android/metadata | grep post-timestamp | cut -d'=' -f2`
 
 $ADB push "$zip_path" "$zip_path_device"
 $ADB shell chgrp cache "$zip_path_device"
